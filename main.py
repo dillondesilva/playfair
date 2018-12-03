151d506497662430f5a120ae3ef6c0cc976c9f43
Dillon de Silva <dillon.desilva@geeksdiary.com>
2018-12-04 07:17:22 +1100

    Wrote function stubs

---
+++ main.py
@@ -0,0 +1,23 @@
+def calcMean (dataset):
+  // This is a stub
+
+def calcMedian (dataset):
+  // This is a stub
+
+def calcMode (dataset):
+  // This is a stub
+
+def calcFrequencies (dataset):
+  // This is a stub
+
+def calcRange (dataset):
+  // This is a stub
+
+def calcUppQuartile (dataset):
+  // This is a stub
+
+def calcLowQuartile (dataset):
+  // This is a stub
+
+def calcInterQuartile (dataset):
+  // This is a stub
